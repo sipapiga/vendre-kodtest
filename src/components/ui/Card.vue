77f8f45145aa4e96c6bb0e2e21ab30ebc6f3cc2d
Pat <sipapat.prasopsap@gmail.com>
2024-05-28 11:01:06 +0200

    Added card component

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card box-shadow">
+    <div class="card-image">
+      <img :src="avatar" :alt="first_name + '-image'" />
+    </div>
+    <div class="card-body">
+      <h3>{{ first_name }} {{ last_name }}</h3>
+      <div class="card-contact flex justify-center">
+        <a  class="gap-4" :href="'mailto:' + email">
+          <ion-icon name="mail-outline"></ion-icon>
+          <span>Kontakt</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
